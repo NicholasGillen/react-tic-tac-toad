5c3ddaf0b84a590bddd5ffe749d87e23034debed
NicholasGillen <ngillen@scottlogic.com>
2023-09-13 11:23:56 +0100

    add animations and new colours

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import './App.css';
 import './logo192.png';
 import { useState } from 'react';
 
-function Square({value, onSquareClick}) {
+
+import victorySound from "./sounds/toadWinner2.mp3";
+import goodChoice from "./sounds/goodChoice.m4a";
+
+function playVictorySound() {
+  new Audio(victorySound).play();
+}
+function playGoodChoice() {
+  new Audio(goodChoice).play();
+}
+
+let winningLineTest;
+
+function Square({id, value, onSquareClick}) {
   let classStatus="square";
   if (value == "X") {
     classStatus = "redToad";
@@ -11,15 +24,24 @@ function Square({value, onSquareClick}) {
     classStatus = "blueToad";
   }
   return (
-    <button className={classStatus} onClick={onSquareClick}></button>
+    <button id={id} className={classStatus} onClick={onSquareClick}></button>
   );
 }
 
 function Board({xIsNext, squares, onPlay}) {
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (squares[i]) {
       return;
     }
+    if (calculateWinner(squares)) {
+      animateWinningLine(winningLineTest);
+      return;
+    }
+    // playVictorySound();
+ 
+      playGoodChoice();
+    
+    
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -31,8 +53,13 @@ function Board({xIsNext, squares, onPlay}) {
   
   const winner = calculateWinner(squares);
   let status;
+  let winningLine;
   if (winner) {
-    status = "Winner: " + winner;
+    playVictorySound();
+    status = "Winner: " + winner[0];
+    winningLine = winner[1];
+    winningLineTest = winningLine;
+    // animateWinningLine(winningLine);
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
@@ -41,19 +68,19 @@ function Board({xIsNext, squares, onPlay}) {
       <div className="status">{status}</div>
       <div className = "boardContainer">
         <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+          <Square id="square0" value={squares[0]} onSquareClick={() => handleClick(0)}/>
+          <Square id="square1" value={squares[1]} onSquareClick={() => handleClick(1)}/>
+          <Square id="square2" value={squares[2]} onSquareClick={() => handleClick(2)}/>
         </div>
         <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+          <Square id="square3" value={squares[3]} onSquareClick={() => handleClick(3)}/>
+          <Square id="square4" value={squares[4]} onSquareClick={() => handleClick(4)}/>
+          <Square id="square5" value={squares[5]} onSquareClick={() => handleClick(5)}/>
         </div>
         <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+          <Square id="square6" value={squares[6]} onSquareClick={() => handleClick(6)}/>
+          <Square id="square7" value={squares[7]} onSquareClick={() => handleClick(7)}/>
+          <Square id="square8" value={squares[8]} onSquareClick={() => handleClick(8)}/>
         </div>
       </div>
     </>
@@ -84,20 +111,23 @@ export default function Game() {
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        <button className="moveButton" onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
   });
 
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+    <>
+      <h1>Tic-Tac-Toad</h1>
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
       </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
-      </div>
-    </div>
+    </>
   );
 }
 
@@ -115,9 +145,40 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      const winningLine = lines[i];
+      return [squares[a], winningLine];
     }
   }
+  return null;
+}
+
+function animateWinningLine(winningLine) {
+  // if (winningLine[0] == 0) {
+  //   let winnerSquare = document.getElementById("square0");
+  //   winnerSquare.classList.add("hidden");
+  // }
+  console.log(winningLine);
+  let winnerOneId = "square";
+  let winnerTwoId = "square";
+  let winnerThreeId = "square";
+
+  winnerOneId += winningLine[0];
+  winnerTwoId += winningLine[1];
+  winnerThreeId += winningLine[2];
+  console.log(winnerOneId);
+  console.log(winnerTwoId);
+  console.log(winnerThreeId);
+
+  let winnerSquare1 = document.getElementById(winnerOneId);
+  let winnerSquare2 = document.getElementById(winnerTwoId);
+  let winnerSquare3 = document.getElementById(winnerThreeId);
+  console.log(winnerSquare1);
+  console.log(winnerSquare2);
+  console.log(winnerSquare3);
+  winnerSquare1.classList.add("victoryAnimation");
+  winnerSquare2.classList.add("victoryAnimation");
+  winnerSquare3.classList.add("victoryAnimation");
+
   return null;
 }
 
